077b0cc354a7b644c00df400583a86b4c60dfc58
mukundesh <mukundesh@outlook.com>
2023-06-15 15:41:43 +0530

    added a missing python file

---
+++ flow/src/text_writer.py
@@ -0,0 +1,90 @@
+import json
+from itertools import groupby
+from operator import itemgetter
+from pathlib import Path
+
+from docint.vision import Vision
+
+
+
+@Vision.factory(
+    "text_writer",
+    default_config={
+        "stub": "textwriter",
+        "output_dir": "output/",
+        "languages": ["en"]
+    },
+)
+class TextWriter:
+    def __init__(self, stub, output_dir, languages):
+        self.conf_dir = Path("conf")
+        self.stub = stub
+        self.output_dir = Path(output_dir)
+        self.languages = languages
+
+    def write_table(self, table_info, lang, lines):
+        if lang == 'mr':
+            total_width = max(len(r['mr']) for r in table_info['rows'])
+            lines.append('-' * total_width)
+            lines.extend(r['mr'].replace('|', ' | ').strip() for r in table_info['rows'])
+            lines.append('-' * total_width)
+            return
+
+        rows = []
+        for row in [r[lang] for r in  table_info['rows']]:
+            rows.append(row.strip('|').split('|'))
+
+        full_rows = [ r for r in rows if len(rows) > 1 ]
+        num_cols = max(len(r) for r in rows)
+        col_widths = [max((len(r[idx]) if idx < len(r) else 0) for r in rows) for idx in range(num_cols)]
+
+        total_width = sum(col_widths) + (3 * (num_cols-1)) + 2 + 2
+        lines.append('-' * total_width)
+        for row in rows:
+            txt_row = [ f'{c:{max(w, 1)}s}' for (c, w) in zip(row, col_widths)]
+            lines.append(f"| {' | '.join(txt_row)} |")
+        lines.append('-' * total_width)
+
+    def build_table_info(self, table, table_trans):
+        row_infos = []
+        for row, row_trans in zip(table.all_rows, table_trans):
+            mr_txt = '|'.join(c.text_with_break() for c in row.cells)
+            en_txt = '|'.join(c for c in row_trans)
+            row_infos.append({'mr': f'|{mr_txt}|', 'en': f'|{en_txt}|'})
+        return { 'rows': row_infos }
+
+    def __call__(self, doc):
+        lang_lines_dict = dict((l, []) for l in self.languages)
+        for page_idx, page in enumerate(doc.pages):
+
+            for (lang, lines) in lang_lines_dict.items():
+                lines.append(f'# Page {page_idx+1}')
+
+            para_table_idx_dict = {}
+            for (para_idx, table_idxs) in groupby(page.table_para_idxs, key=itemgetter(1)):
+                para_table_idx_dict[para_idx] = list(t for (t, p) in table_idxs)
+
+            # add dummy entry, if table is last item
+            for para_idx, para in enumerate(page.paras + [None]): 
+                if para_idx in para_table_idx_dict:
+                    for table_idx in para_table_idx_dict[para_idx]:
+                        t = table_idx
+                        table_info = self.build_table_info(page.tables[t], page.table_trans[t])
+                        
+                        table, table_trans = page.tables[table_idx], page.table_trans[table_idx]
+                        for (lang, lines) in lang_lines_dict.items():
+                            self.write_table(table_info, lang, lines)
+
+                if not para: # dummy entry
+                    continue
+
+                for (lang, lines) in lang_lines_dict.items():
+                    if lang == 'en':
+                        lines.append(page.para_trans[para_idx])
+                    else:
+                        lines.append(para.text_with_break().strip())
+        #end
+        for (lang, lines) in lang_lines_dict.items():
+            lang_file = self.output_dir / f'{doc.pdf_name}.{lang}.txt'
+            lang_file.write_text('\n'.join(lines))
+        return doc

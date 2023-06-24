1de8ae01716890f3e86369dcc301710ec6ff4542
mukundesh <mukundesh@outlook.com>
2023-06-24 19:17:38 +0530

    mv files to orgpedia

--- flow/writeTxt_/src/writeTxt.py
+++ flow/writeTxt_/src/writeTxt.py
@@ -1,47 +1,44 @@
 import sys
-from operator import attrgetter
 from pathlib import Path
 
-import doc_translator  # noqa
 import docint  # noqa
 import orgpedia  # noqa
-import para_finder  # noqa
-import text_writer  # noqa
+
 
 def order_num(pdf_path):
-    org_code, num = pdf_path.stem.rsplit('-', 1)
+    org_code, num = pdf_path.stem.rsplit("-", 1)
     return int(num)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input_path = Path(sys.argv[1])
     output_path = Path(sys.argv[2])
 
-    viz = docint.load('src/writeTxt.yml')
+    viz = docint.load("src/writeTxt.yml")
 
     if input_path.is_dir():
         assert output_path.is_dir()
-        input_files = sorted(input_path.glob('*.pdf'), key=order_num)
+        input_files = sorted(input_path.glob("*.pdf"), key=order_num)
         print(len(input_files))
 
-        #docs = viz.pipe_all(input_files[:3])
-        docs = viz.pipe_all(input_files)        
+        # docs = viz.pipe_all(input_files[:3])
+        docs = viz.pipe_all(input_files)
 
         for doc in docs:
-            output_doc_path = output_path / (doc.pdf_name + '.doc.json')
+            output_doc_path = output_path / (doc.pdf_name + ".doc.json")
             doc.to_disk(output_doc_path)
-    elif input_path.suffix.lower() == '.pdf':
+    elif input_path.suffix.lower() == ".pdf":
         doc = viz(input_path)
         doc.to_disk(output_path)
 
-    elif input_path.suffix.lower() in ('.list', '.lst'):
-        print('processing list')
-        input_files = input_path.read_text().split('\n')
+    elif input_path.suffix.lower() in (".list", ".lst"):
+        print("processing list")
+        input_files = input_path.read_text().split("\n")
 
-        pdf_files = [Path('input') / f for f in input_files if f and f[0] != '#']
+        pdf_files = [Path("input") / f for f in input_files if f and f[0] != "#"]
         pdf_files = [p for p in pdf_files if p.exists()]
 
         docs = viz.pipe_all(pdf_files)
         for doc in docs:
-            output_doc_path = output_path / (doc.pdf_name + '.doc.json')
+            output_doc_path = output_path / (doc.pdf_name + ".doc.json")
             doc.to_disk(output_doc_path)
